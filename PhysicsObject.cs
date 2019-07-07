4869741c15f7c28d8709762ff5159b3acc88de49
unknown <mccannsean421@gmail.com>
2019-07-07 19:35:30 +0100

    Completed horizontal movement

--- PhysicsObject.cs
+++ PhysicsObject.cs
@@ -4,62 +4,67 @@ using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
 {
-
+    
     public float minGroundNormalY = .65f;
-    public float gravityModifier = 1f; // allows to control the gravity 
+    public float gravityModifier = 1f;
 
+    protected Vector2 targetVelocity;
     protected bool grounded;
     protected Vector2 groundNormal;
-
-    /* 
-        Other classes will inherit velocity
-        but we don't want it to be accessible
-        from outside the class
-     */
-     protected Rigidbody2D rb2d;
+    protected Rigidbody2D rb2d;
     protected Vector2 velocity;
-    protected const float minMoveDistance = 0.001f;
-    protected const float shellRadius = 0.1f;
+    protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
     protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
-    private ContactFilter2D contactFilter;
 
 
-    void OnEnable () 
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    void OnEnable()
     {
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    void Start () 
     {
         contactFilter.useTriggers = false;
-        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask (gameObject.layer));
+        contactFilter.SetLayerMask (Physics2D.GetLayerCollisionMask (gameObject.layer));
         contactFilter.useLayerMask = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update () 
     {
-        
+        targetVelocity = Vector2.zero;
+        ComputeVelocity ();    
     }
 
-    void FixedUpdate() 
+    protected virtual void ComputeVelocity()
     {
-        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime; //using default gravity
+
+    }
+
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
 
         grounded = false;
 
-        //predict the next position the object will be in, based on gravity 
-        Vector2 deltaPosition = velocity * Time.deltaTime; //change in position
+        Vector2 deltaPosition = velocity * Time.deltaTime;
 
-        Vector2 move = Vector2.up * deltaPosition.y;
+        Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement (move, false);
+
+        move = Vector2.up * deltaPosition.y;
 
         Movement (move, true);
-    } 
+    }
 
-    void Movement(Vector2 move, bool yMovement) 
+    void Movement(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
 
@@ -99,4 +104,5 @@ public class PhysicsObject : MonoBehaviour
 
         rb2d.position = rb2d.position + move.normalized * distance;
     }
+
 }
